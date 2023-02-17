1849957b85bd0b6cae93b842989b33544f65cf54
RosenPlays17 <122476800+RosenPlays17@users.noreply.github.com>
2023-02-17 23:10:27 +0000

    Add files via upload

---
+++ config.h
@@ -0,0 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "finn";
+static const char *group = "finn";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "#000000",     /* after initialization */
+	[INPUT] =  "#1A1A1A",   /* during input */
+	[FAILED] = "#FF0000",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 0;
